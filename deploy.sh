39debcb7abfc0f319712f23353bafddeecea02ac
sumitpuri <er.sumitpuri@gmail.com>
2020-04-08 08:46:54 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" -X POST https://noderedpuri.mybluemix.net/tririga-outbound -F 'File=@payload1.json'
+curl -H "Content-Type: application/json" -X POST -d '@payload1.json' https://noderedpuri.mybluemix.net/tririga-outbound 
